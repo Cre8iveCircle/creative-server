be8e1eddc4767292858b6d50933ada88b2ac4cf9
Daniel <112815679+danieldaudu65@users.noreply.github.com>
2026-08-01 14:14:28 +0100

    Update nodemailer.js

--- utils/nodemailer.js
+++ utils/nodemailer.js
@@ -15,7 +15,10 @@ const transport = nodemailer.createTransport({
     greetingTimeout: 30000,
     socketTimeout: 30000,
 });
-
+console.log({
+    MAIL_USER: process.env.MAIL_USER,
+    MAIL_PASS: process.env.MAIL_PASS ? "Loaded" : "Missing",
+});
 // ✅ Change this whenever your domain changes
 const TICKET_BASE_URL = "https://cre8ivesummit.online/assets";
 
